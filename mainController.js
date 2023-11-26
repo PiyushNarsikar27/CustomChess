e21c0d8cb78fbce8a1ca9e69429f4523aa40a81a
PiyushNarsikar <piyush.narsikar@tatadigital.com>
2023-11-26 16:14:36 +0530

    Added working draft for main controlling functions

--- mainController.js
+++ mainController.js
@@ -15,22 +15,22 @@ var color;
 var piece1 = {
     id: 1,
     position: {
-        x: 5, y: 6
+        x: 7, y: 1
     },
-    color: color.white,
-    canJump: true,
-    eligibleSquares1: [{ x: 1, y: 4 }, { x: 6, y: 7 }],
+    color: color.black,
+    canJump: false,
+    eligibleSquares1: [{ x: 7, y: 0 }, { x: 6, y: 2 }],
     eligibleSquares2: [{ x: 2, y: 4 }, { x: 6, y: 7 }],
     eligibleSquares3: [{ x: 2, y: 4 }, { x: 6, y: 7 }]
 };
 var piece2 = {
     id: 1,
     position: {
-        x: 5, y: 6
+        x: 6, y: 3
     },
     color: color.black,
     canJump: true,
-    eligibleSquares1: [{ x: 2, y: 4 }, { x: 6, y: 7 }],
+    eligibleSquares1: [{ x: 7, y: 1 }, { x: 6, y: 7 }],
     eligibleSquares2: [{ x: 2, y: 4 }, { x: 6, y: 7 }],
     eligibleSquares3: [{ x: 2, y: 4 }, { x: 6, y: 7 }]
 };
@@ -39,20 +39,20 @@ var piece3 = {
     position: {
         x: 5, y: 6
     },
-    color: color.black,
+    color: color.white,
     canJump: true,
-    eligibleSquares1: [{ x: 2, y: 4 }, { x: 6, y: 7 }],
+    eligibleSquares1: [{ x: 2, y: 4 }, { x: 6, y: 2 }],
     eligibleSquares2: [{ x: 2, y: 4 }, { x: 6, y: 7 }],
     eligibleSquares3: [{ x: 2, y: 4 }, { x: 6, y: 7 }]
 };
 var pieceKingW = {
     id: 1,
     position: {
-        x: 5, y: 6
+        x: 7, y: 0
     },
     color: color.white,
     canJump: true,
-    eligibleSquares1: [{ x: 2, y: 4 }, { x: 6, y: 7 }],
+    eligibleSquares1: [{ x: 7, y: 1 }, { x: 7, y: 5 }],
     eligibleSquares2: [{ x: 2, y: 4 }, { x: 6, y: 7 }],
     eligibleSquares3: [{ x: 2, y: 4 }, { x: 6, y: 7 }]
 };
@@ -71,23 +71,84 @@ var board = [];
 for (var i = 0; i < 8; i++) {
     board.push([]);
     for (var j = 0; j < 8; j++) {
+        if (i == 7 && j == 0) {
+            board[i].push(pieceKingW);
+        }
         board[i].push(null);
     }
 }
 var turn = color.white;
 var alivePiecesList = [piece1, piece2, piece3, pieceKingB, pieceKingW];
-function getCheckersEligibleSquares(initialPosition, intendedPosition, whichKing) {
+function getCheckersEligibleSquares(whichKing, initialPosition, intendedPosition) {
     for (var j = 0; j < alivePiecesList.length; j++) {
         if (alivePiecesList[j].color != whichKing.color) {
             for (var i = 0; i < alivePiecesList[j].eligibleSquares1.length; i++) {
                 if (whichKing.position.x == alivePiecesList[j].eligibleSquares1[i].x && whichKing.position.y == alivePiecesList[j].eligibleSquares1[i].y) { //eligibleSquare calc changes based on initial and intended positions
-                    var checkPath = alivePiecesList[j].position;
-                    return __spreadArray([checkPath], alivePiecesList[j].eligibleSquares1, true);
+                    console.log(j);
+                    var checkersEligibleSquares = alivePiecesList[j].position;
+                    return __spreadArray([checkersEligibleSquares], alivePiecesList[j].eligibleSquares1, true);
                 }
             }
         }
     }
     return [];
 }
-var x = getCheckersEligibleSquares({ x: 5, y: 0 }, { x: 7, y: 4 }, pieceKingB);
-console.log(x);
+function checkForCheckmate() {
+    var whichKing;
+    if (turn == color.black) {
+        whichKing = pieceKingB;
+    }
+    else {
+        whichKing = pieceKingW;
+    }
+    var checkPath = getCheckersEligibleSquares(whichKing);
+    if (checkPath.length == 0 || whichKing.eligibleSquares1.length != 0) {
+        return false;
+    }
+    if ((board[checkPath[0].x][checkPath[0].y]).canJump) {
+        return true;
+    }
+    for (var i = 0; i < alivePiecesList.length; i++) {
+        if (alivePiecesList[i].color == turn) {
+            var pieceAccessSquares = alivePiecesList[i].eligibleSquares1;
+            for (var j = 0; j < checkPath.length; j++) {
+                for (var k = 0; k < pieceAccessSquares.length; k++) {
+                    if (pieceAccessSquares[k].x == checkPath[j].x && (pieceAccessSquares[k].y == checkPath[j].y)) { // Add move possible check
+                        return false;
+                    }
+                }
+            }
+        }
+    }
+    return true;
+}
+function isMoveLegal(playerId, initialPosition, finalPosition) {
+    //playerToMoveIsMovingCheck()
+    //moveOutOfTheBoardCheck()
+    var piece = board[initialPosition.x][initialPosition.y];
+    if ((piece === null || piece === void 0 ? void 0 : piece.color) != turn) {
+        return false;
+    }
+    var isInEligibleSquares = false;
+    for (var i = 0; i < piece.eligibleSquares1.length; i++) {
+        if (piece.eligibleSquares1[i].x == finalPosition.x && piece.eligibleSquares1[i].y == finalPosition.y) {
+            isInEligibleSquares = true;
+            break;
+        }
+    }
+    if (!isInEligibleSquares) {
+        return false;
+    }
+    var whichKing;
+    if (turn == color.black) {
+        whichKing = pieceKingB;
+    }
+    else {
+        whichKing = pieceKingW;
+    }
+    if (piece == whichKing || getCheckersEligibleSquares(whichKing, initialPosition, finalPosition).length == 0) {
+        return true;
+    }
+    return false;
+}
+console.log(isMoveLegal(1, { x: 7, y: 0 }, { x: 7, y: 5 }));
--- mainController.ts
+++ mainController.ts
@@ -21,11 +21,11 @@ const piece1: {
     } = {
         id: 1,
         position: {
-            x: 5, y: 6
+            x: 7, y: 1
         },
-        color: color.white,
-        canJump: true,
-        eligibleSquares1: [{x:1,y:4}, {x:6,y:7}],
+        color: color.black,
+        canJump: false,
+        eligibleSquares1: [{x:7,y:0}, {x:6,y:2}],
         eligibleSquares2: [{x:2,y:4}, {x:6,y:7}],
         eligibleSquares3: [{x:2,y:4}, {x:6,y:7}]
     }
@@ -49,11 +49,11 @@ const piece1: {
         } = {
             id: 1,
             position: {
-                x: 5, y: 6
+                x: 6, y: 3
             },
             color: color.black,
             canJump: true,
-            eligibleSquares1: [{x:2,y:4}, {x:6,y:7}],
+            eligibleSquares1: [{x:7,y:1}, {x:6,y:7}],
             eligibleSquares2: [{x:2,y:4}, {x:6,y:7}],
             eligibleSquares3: [{x:2,y:4}, {x:6,y:7}]
         }
@@ -80,9 +80,9 @@ const piece1: {
                 position: {
                     x: 5, y: 6
                 },
-                color: color.black,
+                color: color.white,
                 canJump: true,
-                eligibleSquares1: [{x:2,y:4}, {x:6,y:7}],
+                eligibleSquares1: [{x:2,y:4}, {x:6,y:2}],
                 eligibleSquares2: [{x:2,y:4}, {x:6,y:7}],
                 eligibleSquares3: [{x:2,y:4}, {x:6,y:7}]
             }
@@ -107,11 +107,11 @@ const piece1: {
                 } = {
                     id: 1,
                     position: {
-                        x: 5, y: 6
+                        x: 7, y: 0
                     },
                     color: color.white,
                     canJump: true,
-                    eligibleSquares1: [{x:2,y:4}, {x:6,y:7}],
+                    eligibleSquares1: [{x:7, y:1}, {x:7, y:5}],
                     eligibleSquares2: [{x:2,y:4}, {x:6,y:7}],
                     eligibleSquares3: [{x:2,y:4}, {x:6,y:7}]
                 }
@@ -146,10 +146,13 @@ const piece1: {
                     
      
 
-var board: typeof piece1 | null[][] = []
+var board: (typeof piece1 | null)[][] = []
 for(var i=0;i<8;i++){
     board.push([])
     for(var j=0;j<8;j++){
+        if(i==7 && j==0){
+            board[i].push(pieceKingW)
+        }
         board[i].push(null)
     }
 }
@@ -159,8 +162,8 @@ var turn: color = color.white
 
 var alivePiecesList: typeof piece1[] = [piece1, piece2, piece3, pieceKingB, pieceKingW]
 
-function getCheckersEligibleSquares(initialPosition: typeof piece1.position, 
-    intendedPosition: typeof piece1.position,whichKing: typeof piece1): typeof piece1.eligibleSquares1{
+function getCheckersEligibleSquares(whichKing: typeof piece1, initialPosition?: typeof piece1.position, 
+    intendedPosition?: typeof piece1.position): typeof piece1.eligibleSquares1{
     for(var j=0;j<alivePiecesList.length;j++){
         if(alivePiecesList[j].color!=whichKing.color){
             for(var i=0;i<alivePiecesList[j].eligibleSquares1.length;i++){
@@ -174,3 +177,62 @@ function getCheckersEligibleSquares(initialPosition: typeof piece1.position,
     return []
 }
 
+function checkForCheckmate(): boolean{
+    var whichKing: typeof piece1
+    if(turn==color.black){
+        whichKing=pieceKingB
+    } else{
+        whichKing=pieceKingW
+    }
+    var checkPath: typeof piece1.eligibleSquares1=getCheckersEligibleSquares(whichKing)
+    if(checkPath.length==0 || whichKing.eligibleSquares1.length!=0){
+        return false
+    }
+    if((board[checkPath[0].x][checkPath[0].y]!).canJump){
+        return true
+    }
+    for(var i=0;i<alivePiecesList.length;i++){
+        if(alivePiecesList[i].color==turn){
+            var pieceAccessSquares=alivePiecesList[i].eligibleSquares1
+            for(var j=0;j<checkPath.length;j++){
+                for(var k=0;k<pieceAccessSquares.length;k++){
+                    if(pieceAccessSquares[k].x==checkPath[j].x && (pieceAccessSquares[k].y==checkPath[j].y)){ // Add move possible check
+                        return false
+                    }
+                }
+            }
+        }
+    }
+    return true
+}
+
+function isMoveLegal(playerId: number, initialPosition: typeof piece1.position, finalPosition: typeof piece1.position): boolean{
+    //playerToMoveIsMovingCheck()
+    //moveOutOfTheBoardCheck()
+    var piece = board[initialPosition.x][initialPosition.y]
+    if(piece?.color!=turn){
+        return false
+    }
+    var isInEligibleSquares=false
+    for(var i=0;i<piece.eligibleSquares1.length;i++){
+        if(piece.eligibleSquares1[i].x==finalPosition.x && piece.eligibleSquares1[i].y==finalPosition.y){
+            isInEligibleSquares = true
+            break
+        }
+    }
+    if(!isInEligibleSquares){
+        return false
+    }
+    var whichKing: typeof piece1
+    if(turn==color.black){
+        whichKing=pieceKingB
+    } else{
+        whichKing=pieceKingW
+    }
+    if(piece==whichKing || getCheckersEligibleSquares(whichKing, initialPosition, finalPosition).length==0){
+        return true
+    }
+    return false
+}
+
+console.log(isMoveLegal(1, {x:7, y:0}, {x:7, y:5}))
